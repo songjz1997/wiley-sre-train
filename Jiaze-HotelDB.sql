34e190fe13da5fa87dd1496987a17a940446c29d
songjz1997 <105316717+songjz1997@users.noreply.github.com>
2022-08-06 14:16:05 +0800

    Update Jiaze-HotelDB.sql

--- Jiaze-HotelDB.sql
+++ Jiaze-HotelDB.sql
@@ -6,24 +6,25 @@ CREATE DATABASE HotelDB;
 USE HotelDB;
 
 CREATE TABLE Room (
-	RoomId int PRIMARY KEY,
+    RoomId int PRIMARY KEY,
     RoomType VARCHAR(10) NOT NULL,
-    ADA_Accessible BOOL NOT NULL DEFAULT 1,
+    ADA_Accessible varchar(10) not null,
     Standard_Occupancy int not NULL,
     Max_Occupancy int not null,
     Base_price float not null,
-    Extra_person_price float not null,
-    Is_microwave BOOL NOT NULL DEFAULT 0,
-    Is_jacuzzi BOOL NOT NULL DEFAULT 0,
-    Is_refrigerator BOOL NOT NULL DEFAULT 0
+    Extra_person_price float,
+    Is_microwave varchar(10) not null,
+    Is_jacuzzi varchar(10) not null,
+    Is_refrigerator varchar(10) not null,
+    Is_oven varchar(10) not null
 );
 
 CREATE TABLE Guest (
-	GuestId int PRIMARY KEY,
+    GuestId int PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
     Address VARCHAR(100) NOT NULL,
     City VARCHAR(30) not NULL,
-    State int not null,
+    State VARCHAR(30) not null,
     Zip int,
     Phone VARCHAR(20) not null
 );
